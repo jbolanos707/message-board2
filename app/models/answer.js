9ea607bcc368174fea06a91e6a5a88199105dacc
Jennifer Bolanos <jbolanos707@gmail.com>
2015-06-24 21:24:39 -0700

    create a question model and answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,1 +1,7 @@
+import DS from 'ember-data';
 
+export default DS.Model.extend({
+  date: DS.attr('string'),
+  answer_body: DS.attr('string'),
+  question: DS.belongsTo('question', {async: true})
+});
--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-
+  Router.map(function() {
+    this.resource('questions', {path: "/"});
+  });
 });
 
 
